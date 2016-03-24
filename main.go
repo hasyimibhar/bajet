3fa562eea63038428cf380cd3f6f8ad0da267eec
Hasyimi Bahrudin <hasyimibahrudin@gmail.com>
2016-03-25 00:33:34 +0800

    Allow user to add items, which are stored in memory

--- main.go
+++ main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+    "log"
     "net/http"
 
     "github.com/gorilla/mux"
+    "github.com/gorilla/schema"
 )
 
+type Item struct {
+    Description string
+    Cost        int
+}
+
+var Items []Item
+
 func AddItemHandler(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()
+
+    decoder := schema.NewDecoder()
+    item := Item{}
+    decoder.Decode(&item, r.PostForm)
+
+    Items = append(Items, []Item{item}...)
+    log.Println("[TRACE] Items:", Items)
+
     http.Redirect(w, r, "/", http.StatusMovedPermanently)
 }
 
 func main() {
+    Items = []Item{}
+
     r := mux.NewRouter()
     r.HandleFunc("/items", AddItemHandler).Methods(http.MethodPost)
     r.PathPrefix("/").Handler(http.FileServer(http.Dir("./public/")))
